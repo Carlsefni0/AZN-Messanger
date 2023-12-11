12bf4a9dff90d77bf06383188724599adc639b0b
Nazar <example"gmail.com>
2023-12-11 20:37:00 +0200

    Code refactoring was performed

--- src/Components/LogIn.jsx
+++ src/Components/LogIn.jsx
@@ -1,6 +1,8 @@
+/* eslint-disable react/prop-types */
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import logo from "../assets/Img/logo.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPaperPlane } from "@fortawesome/free-solid-svg-icons";
 
 function LogIn({ callBack }) {
   const [Email, setEmail] = useState(undefined);
@@ -8,7 +10,6 @@ function LogIn({ callBack }) {
   function changeEmail(e) {
     e.preventDefault();
     setEmail(e.target.value);
-    console.log(Email);
   }
   async function sendEmail() {
     const fetchParametres = {
@@ -18,28 +19,27 @@ function LogIn({ callBack }) {
       },
       body: JSON.stringify(Email),
     };
-    const response = await fetch("http://25.29.145.192:8080/api/v1/user", fetchParametres);
+    const response = await fetch("http://192.168.43.246:8080/api/v1/user", fetchParametres);
     if (response.ok) {
       const data = await response.json();
 
-      console.log(data.id);
       navigate("/WorkSpace");
       callBack({ login: Email, id: data.id });
     } else {
-      console.log("Error");
+      alert("Error");
     }
   }
   return (
     <div className="bg-[url('./assets/Img/bg.jpg')]  w-screen h-screen pt-20">
-      <div className="w-[80px] h-[80px] bg-slate-400 m-auto mb-[35px] rounded-full flex items-center shadow">
-        <img src={logo} className="w-10 h-10 m-auto "></img>
+      <div className="w-[80px] h-[80px] bg-gradient-to-t from-slate-500 to-slate-600 m-auto mb-[35px] rounded-full flex items-center shadow border-slate-400 border-2 ">
+        <FontAwesomeIcon className="text-4xl text-slate-300 px-4" icon={faPaperPlane} />
       </div>
-      <div className="w-[420px] h-[500px] shadow rounded-xl bg-slate-400 block m-auto pt-[5px]">
-        <h1 className="text-3xl font-mono text-center text-slate-250 font-bold">Welcome!</h1>
+      <div className="w-[420px] h-[500px] shadow rounded-xl bg-gradient-to-t from-slate-500 to-slate-600  border-slate-400 border-2 block m-auto pt-[5px]">
+        <h1 className="text-3xl font-mono text-center text-slate-250 font-bold text-slate-300">Welcome!</h1>
         <form>
           <div className="ml-[40px] mt-[40px]">
-            <label className="text-2ml font-bold"> Email</label>
-            <input className="block  mt-[10px] w-[340px] h-[30px] shadow rounded bg-slate-50 border-slate-50 " type="email" placeholder="Email" onChange={changeEmail}></input>
+            <label className="text-2ml font-bold text-slate-300"> Email</label>
+            <input className="block  mt-[10px] w-[340px] h-[30px] shadow rounded disabled:opacity-75 border-none " type="email" placeholder="Email" onChange={changeEmail}></input>
           </div>
         </form>
         <button className=" block m-auto mt-[190px] rounded-sm bg-slate-600 w-[240px] h-[50px] hover:bg-slate-300 duration-500" onClick={sendEmail}>
